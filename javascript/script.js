2a2971144511b13fc6f687fcb9df6c3f23816081
udehsuccess <udehsuccess2@gmail.com>
2021-10-03 02:33:30 +0100

    work done

---
+++ javascript/script.js
@@ -0,0 +1,71 @@
+  //TMDB 
+
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
+getMovies(API_URL);
+
+function getMovies(url) {
+  fetch(url).then(res => res.json()).then(data => {
+   console.log(data.results);
+    showMovies(data.results);
+
+  });
+
+}
+
+function showMovies(data) {
+  main.innerHTML = '';
+
+
+  data.forEach(movie => {
+    const {title, poster_path, vote_average, overview} = movie;
+    const movie1 = document.createElement('div');
+    movie1.classList.add('movie');
+    movie1.innerHTML = `
+    <img src="${IMG_URL+poster_path}" alt="${title}">
+
+    <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getColor(vote_average)}">${vote_average}</span>
+    </div>
+
+    <div class="overview">
+        <h1>Overview</h1>
+        ${overview}
+    </div>
+
+    `
+
+    main.appendChild(movie1);
+  })
+}
+
+function getColor(vote){
+  if(vote>= 8){
+    return 'green'
+  }else if(vote >= 5){
+    return "orange"
+  }else{
+    return 'red'
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if(searchTerm) {
+    getMovies(searchURL+'&query='+searchTerm)
+  }else{
+    getMovies(API_URL);
+  }
+})
